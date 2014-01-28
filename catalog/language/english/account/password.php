9c8f3b7d9d659bbaae6ea94a53433b1b3c3fa847
powercatfish <kip@thebabyshop.com>
2014-01-28 01:01:38 -0800

    update the wording in member reset password page

--- catalog/language/english/account/password.php
+++ catalog/language/english/account/password.php
@@ -4,12 +4,12 @@ $_['heading_title']  = 'Change Password';
 
 // Text
 $_['text_account']   = 'Account';
-$_['text_password']  = 'Your Password';
+$_['text_password']  = 'Change Password';
 $_['text_success']   = 'Success: Your password has been successfully updated.';
 
 // Entry
-$_['entry_password'] = 'Password:';
-$_['entry_confirm']  = 'Password Confirm:';
+$_['entry_password'] = 'New Password:';
+$_['entry_confirm']  = 'New Password Confirm:';
 
 // Error
 $_['error_password'] = 'Password must be between 4 and 20 characters!';
