586ba7d808fe62f831b6c7678a4e947c57de2fa6
powercatfish <kip@thebabyshop.com>
2014-02-10 17:27:14 -0800

    GA Code update by OP Hub

--- catalog/controller/checkout/success.php
+++ catalog/controller/checkout/success.php
@@ -1,6 +1,6 @@
 <?php
-class ControllerCheckoutSuccess extends Controller { 
-	public function index() { 	
+class ControllerCheckoutSuccess extends Controller {
+	public function index() {
 		if (isset($this->session->data['order_id'])) {
 			$this->cart->clear();
 
@@ -10,37 +10,37 @@ class ControllerCheckoutSuccess extends Controller {
 			unset($this->session->data['payment_methods']);
 			unset($this->session->data['guest']);
 			unset($this->session->data['comment']);
-			unset($this->session->data['order_id']);	
+			unset($this->session->data['order_id']);
 			unset($this->session->data['coupon']);
 			unset($this->session->data['reward']);
 			unset($this->session->data['voucher']);
 			unset($this->session->data['vouchers']);
-		}	
-									   
+		}
+
 		$this->language->load('checkout/success');
-		
+
 		$this->document->setTitle($this->language->get('heading_title'));
-		
-		$this->data['breadcrumbs'] = array(); 
+
+		$this->data['breadcrumbs'] = array();
 
       	$this->data['breadcrumbs'][] = array(
         	'href'      => $this->url->link('common/home'),
         	'text'      => $this->language->get('text_home'),
         	'separator' => false
-      	); 
-		
+      	);
+
       	$this->data['breadcrumbs'][] = array(
         	'href'      => $this->url->link('checkout/cart'),
         	'text'      => $this->language->get('text_basket'),
         	'separator' => $this->language->get('text_separator')
       	);
-				
+
 		$this->data['breadcrumbs'][] = array(
 			'href'      => $this->url->link('checkout/checkout', '', 'SSL'),
 			'text'      => $this->language->get('text_checkout'),
 			'separator' => $this->language->get('text_separator')
-		);	
-					
+		);
+
       	$this->data['breadcrumbs'][] = array(
         	'href'      => $this->url->link('checkout/success'),
         	'text'      => $this->language->get('text_success'),
@@ -48,13 +48,13 @@ class ControllerCheckoutSuccess extends Controller {
       	);
 
 		$this->data['heading_title'] = $this->language->get('heading_title');
-		
+
 		if ($this->customer->isLogged()) {
     		$this->data['text_message'] = sprintf($this->language->get('text_customer'), $this->url->link('account/account', '', 'SSL'), $this->url->link('account/order', '', 'SSL'), $this->url->link('account/download', '', 'SSL'), $this->url->link('information/contact'));
 		} else {
     		$this->data['text_message'] = sprintf($this->language->get('text_guest'), $this->url->link('information/contact'));
 		}
-		
+
     	$this->data['button_continue'] = $this->language->get('button_continue');
 
     	$this->data['continue'] = $this->url->link('common/home');
@@ -64,16 +64,32 @@ class ControllerCheckoutSuccess extends Controller {
 		} else {
 			$this->template = 'default/template/common/success.tpl';
 		}
-		
+
 		$this->children = array(
 			'common/column_left',
 			'common/column_right',
 			'common/content_top',
 			'common/content_bottom',
 			'common/footer',
-			'common/header'			
+			'common/header'
 		);
-				
+
+		// Add data for
+		$query = $this->db->query("SELECT order_id, total FROM " . DB_PREFIX . "order WHERE order_id = (SELECT MAX(order_id) FROM store_order WHERE customer_id=".$this->customer->getId().")");
+		foreach ($query->rows as $result) {
+			$order_id = $result['order_id'];
+			$total = $result['total'];
+			$this->data['transaction'] = json_encode( array( 'id' => $order_id, 'affiliation' => 'The Baby Shop', 'revenue' => $total, 'shipping' => '0', 'tax' => '0' ) );
+
+			$query2 = $this->db->query("SELECT order_product_id, name, model, quantity, price FROM " . DB_PREFIX . "order_product WHERE order_id = $order_id");
+			$items = array();
+			foreach ($query2->rows as $result) {
+				$items[$result['order_product_id']] = array( 'id' => $order_id, 'name'=> $result['name'], 'sku' => $result['model'], 'price' => $result['price'], 'quantity' => $result['quantity'] );
+			}
+			$this->data['items'] = json_encode( $items );
+			break;
+		}
+
 		$this->response->setOutput($this->render());
   	}
 }
--- catalog/view/theme/thebabyshop/template/common/success.tpl
+++ catalog/view/theme/thebabyshop/template/common/success.tpl
@@ -1,22 +1,4 @@
 <?php echo $header; ?>
-<!-- Google Code for Remarketing Tag -->
-<!--------------------------------------------------
-Remarketing tags may not be associated with personally identifiable information or placed on pages related to sensitive categories. See more information and instructions on how to setup the tag on: http://google.com/ads/remarketingsetup
---------------------------------------------------->
-<script type="text/javascript">
-/* <![CDATA[ */
-var google_conversion_id = 993256659;
-var google_custom_params = window.google_tag_params;
-var google_remarketing_only = true;
-/* ]]> */
-</script>
-<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
-</script>
-<noscript>
-<div style="display:inline;">
-<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/993256659/?value=0&amp;guid=ON&amp;script=0"/>
-</div>
-</noscript>
 <?php echo $column_left; ?><?php echo $column_right; ?>
 <div id="content" class="span13"><div class="row-fluid">
   <h1><?php echo $heading_title; ?></h1>
@@ -25,4 +7,13 @@ var google_remarketing_only = true;
     <div class="right"><a href="<?php echo $continue; ?>" class="button"><?php echo $button_continue; ?></a></div>
   </div>
   <?php echo $content_bottom; ?></div></div>
+<script>(function(){ 'use strict'; 
+ga('require', 'ecommerce', 'ecommerce.js');
+ga('ecommerce:addTransaction', <?php echo $transaction ?> );
+var items = <?php echo $items ?>;
+for ( var i in items ) {
+   ga('ecommerce:addItem', items[i] );
+}
+ga('ecommerce:send');
+})();</script>
 <?php echo $footer; ?>
